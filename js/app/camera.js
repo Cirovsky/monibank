1687ff9def76c4098c6f34f8dff0933156bb6276
Cirovsky <cirovisqui@gmail.com>
2023-03-10 15:41:24 -0300

    finalizado sistema de captura da imagem da camera

--- js/app/camera.js
+++ js/app/camera.js
@@ -1,6 +1,12 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviar = document.querySelector("[data-enviar]");
+let imagemURL = "";
+
 
 botaoIniciarCamera.addEventListener("click", async function () {
     const iniciarvideo = await navigator.mediaDevices
@@ -11,3 +17,23 @@ botaoIniciarCamera.addEventListener("click", async function () {
 
     video.srcObject = iniciarvideo;
 });
+
+botaoTirarFoto.addEventListener("click", function (){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+    imagemURL = canvas.toDataURL('image/jpeg');
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
+});
+
+botaoEnviar.addEventListener("click", ()=> {
+    const dadosExistentes = localStorage.getItem("cadastro");
+    const converteDados = JSON.parse(dadosExistentes);
+    
+    converteDados.imagem = imagemURL;
+
+    localStorage.setItem("cadastro", JSON.stringify(converteDados));
+
+    window.location.href ='./abrir-conta-form-3.html';
+})
